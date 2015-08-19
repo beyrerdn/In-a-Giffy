fe262971859ec6ae94601a823a4457860ac5474a
Daniel Beyrer <beyrerdn@gmail.com>
2015-08-18 20:52:14 -0400

    Added to the integration test for login and logout.

--- test/integration/user_accounts_test.rb
+++ test/integration/user_accounts_test.rb
@@ -6,13 +6,35 @@ class UserAccountsTest < ActionDispatch::IntegrationTest
     Capybara.current_driver = :poltergeist
     visit("/")
     click_link("Sign Up")
-    fill_in("Email", :with => "beyrerdn@inagiffy.com"
+    fill_in("Email", :with => "beyrerdn@inagiffy.com")
     fill_in("Username", :with => "beyrerdn")
     fill_in("Password", :with => "password")
     fill_in("Password confirmation", :with => "password")
     click_on("Sign up")
     assert pag.has_content?('Logout')
-    )
+  end
+
+  test "signing in" do
+    Capybara.current_driver = :poltergeist
+    visit("/")
+    click_link("Login")
+    fill_in("Email", :with => "beyrerdn@inagiffy.com")
+    fill_in("Password", :with => "password")
+    fill_in("Password confirmation", :with => "password")
+    click_on("Log in")
+    assert pag.has_content?('Logout')
+  end
+
+  test "logging out" do
+    Capybara.current_driver = :poltergeist
+    visit("/")
+    click_link("Login")
+    fill_in("Email", :with => "beyrerdn@inagiffy.com")
+    fill_in("Password", :with => "password")
+    fill_in("Password confirmation", :with => "password")
+    click_on("Log in")
+    click_on("Logout")
+    assert pag.has_content?('Login')
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,15 @@
 require 'simplecov'
 SimpleCov.start
 
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+require 'capybara/rails'
+
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
 end
-
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
